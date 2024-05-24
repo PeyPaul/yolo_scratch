62505e10c1d6ed1fc79c7bdcb4c0a244842232e6
Paul Peytevin <ppeytevin@gmail.com>
2024-05-24 13:10:44 +0200

    start of dataset.py

---
+++ dataset.py
@@ -0,0 +1,14 @@
+import torch
+import os
+import pandas as pd
+from PIL import Image
+
+class VOCDataset(torch.utils.data.Dataset):
+    def __init__(self, csv_file, img_dir, label_dir, S=7, B=2, C=20, transform=None):
+        self.annotations = pd.read_csv(csv_file)
+        self.img_dir = img_dir
+        self.label_dir = label_dir
+        self.transform = transform
+        self.S = S
+        self.B = B
+        self.C = C
