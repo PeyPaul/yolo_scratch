55194d3ea06d4310b5c7c2619aa1d13c823e5563
Paul Peytevin <ppeytevin@gmail.com>
2024-06-02 13:22:37 +0200

    refactor: Update VOCDataset class in dataset.py

--- dataset.py
+++ dataset.py
@@ -35,4 +35,21 @@ class VOCDataset(torch.utils.data.Dataset):
             image, boxes = self.transform(image, boxes)
         
         # Convert To Cells
-        label_matrix = torch.zeros((self.S, self.S, self.C + 5 * self.B))
+        label_matrix = torch.zeros((self.S, self.S, self.C + 5 * self.B)) # maybe we will nedd to remove '* self.B'
+        for box in boxes:
+            class_label, x, y, width, height = box.tolist()
+            class_label = int(class_label)
+            i, j = int(self.S * y), int(self.S * x)
+            x_cell, y_cell = self.S * x - j, self.S * y - i
+            width_cell, height_cell = (
+                width * self.S,
+                height * self.S
+            )
+            
+            if label_matrix[i, j, 20] == 0:
+                label_matrix[i, j, 20] = 1
+                box_coordinates = torch.tensor([x_cell, y_cell, width_cell, height_cell])
+                label_matrix[i, j, 21:25] = box_coordinates
+                label_matrix[i, j, class_label] = 1
+                
+        return image, label_matrix
